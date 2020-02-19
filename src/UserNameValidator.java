2f8e50410bd0f62b7c340c54a952818d079f3540
yashmeen <yashmeensharma746@gmail.ca>
2020-02-19 18:46:17 -0500

    Yashmeen ICE2

---
+++ src/UserNameValidator.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+import midtermreviewcodeforpartc.User;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yashmeen Sharma
+ */
+public class UserNameValidator {
+    private void run()
+    {
+        int userCount=0;//keep track of number of users for array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter your desired user name:");
+        String userName = sc.nextLine();
+        boolean validusername=false;
+        String password="";
+        while(!validusername)
+        {
+            System.out.println("Passwords must have at least onr character");
+            
+            password = sc.nextLine();
+            int specialCharCount=0;
+           
+            for(int i=0;i<username.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&username.length()>=1)
+            {
+                validusername=true;
+            }
+        }
+        
+        User newUser = new User(userName, password);
+        System.out.println("UserName: " + userName);
+        
+    }//end run method
+   
+    
+}//end class
+    
+
